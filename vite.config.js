bbf2f450ffb50f21e198be81e1fa90d8a9f95c10
Aviles17 <santiaviles17@gmail.com>
2024-04-04 10:08:03 -0500

    fix: Add base url to vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Aviles17.github.io/My_react_resume/'
 })
+
